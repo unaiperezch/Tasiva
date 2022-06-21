c5f49b4c10182b733fd88eab086d3ab883f58906
UnaiPerezDAW2 <uperezchic@educacion.navarra.es>
2022-06-21 19:28:22 +0200

    Implementación Header

--- src/views/header.php
+++ src/views/header.php
@@ -4,11 +4,73 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="src\styles\footer_styles.css">
+    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <title>Tasiva</title>
+    
+    <title>Tavisa</title>
 </head>
 <body>
+<header>
+    <nav class="navbar navbar-dark bg-dark">
+        <li class="nav-item dropdown" style="list-style-type: none;">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Soluciones</a>
+
+            <div class="dropdown-menu">
+            <h5 class="dropdown-header">Servicio</h5>
+            <a class="dropdown-item">Servicios</a>
+            <a class="dropdown-item">Procesos</a>
+            <a class="dropdown-item">Solución única</a>
+
+            <div class="dropdown-divider"></div>
+            <h5 class="dropdown-header">Producto</h5>
+            <a class="dropdown-item">Estandarizacíon</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown" style="list-style-type: none;">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Somos</a>
+
+            <div class="dropdown-menu">
+            <h5 class="dropdown-header">Equipo</h5>
+            <a class="dropdown-item">Personas</a>
+            <a class="dropdown-item">Javier</a>
+            
+            <div class="dropdown-divider"></div>
+            <h5 class="dropdown-header">Empresa</h5>
+            <a class="dropdown-item">Misíon</a>
+            <a class="dropdown-item">Visión</a>
+            <a class="dropdown-item">Valores</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown" style="list-style-type: none;">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Sectores</a>
+
+            <div class="dropdown-menu">
+            <a class="dropdown-item">Agroalimentario</a>
+            <a class="dropdown-item">Automóvil</a>
+            <a class="dropdown-item">Alimentación</a>
+            <a class="dropdown-item">Farmacéutico</a>
+            <a class="dropdown-item">Plástico</a>
+            <a class="dropdown-item">Otros</a>
+            </div>
+        </li>
+
+        <li class="nav-item dropdown" style="list-style-type: none;">
+            <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown">Talento</a>
+
+            <div class="dropdown-menu">
+            <a class="dropdown-item">Por qué buscamos</a>
+            <a class="dropdown-item">Cómo conocernos</a>
+            </div>
+        </li>
+
+        <li class="nav-item" style="list-style-type: none;">
+            <a class="nav-link" href="#">Contacto</a>
+        </li>
+    </nav> 
+</header>
